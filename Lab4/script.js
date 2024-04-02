19027cf0b3b184faf3f7c09f88a428addf49a131
shurikek3990 <shurik.shelest@gmail.com>
2024-04-02 15:23:02 +0200

    .

---
+++ Lab4/script.js
@@ -0,0 +1,56 @@
+/* a)
+function NYcountdown(n){
+    let i ;
+    for (i=n; i>0; i--){
+        console.log(i);
+    }
+    if(i == 0){
+        console.log("Happy New Year, BayBay")
+    }
+}
+NYcountdown()
+*/
+
+/* b)
+function NYcountdown(n){
+    let i=n ;
+    while(i>0) {
+        i--;
+        console.log(i);
+    }
+    if(i==0){console.log("Happy New Year, BAYBAY")
+    }
+}
+NYcountdown()
+*/
+/* ZAD2
+function factorial(n){
+    let answer = 1;
+    if (n == 0 || n == 1){
+      return answer;
+    }
+    else if(n > 1){
+      for(var i = n; i >= 1; i--){
+        answer = answer * i;
+      }
+      return answer;
+    }
+    else{
+      return "number has to be positive."
+    }  
+  }
+  let n = prompt("Give me da numbah, brudda");
+  answer = factorial(n)
+  console.log("Factorial of " + n + " : " + answer);
+  */
+ function jschlatt1999myway6peopledead() {
+    age = prompt("What's your age, brudda?");
+    if (age>=18) {
+        alert("Welcome brudda!")
+    }
+    else{
+        alert("No no no, brudda. You are going to kids site");
+        window.open("https://en.wikipedia.org/wiki/Enumclaw_horse_sex_case")
+
+    }
+ }
